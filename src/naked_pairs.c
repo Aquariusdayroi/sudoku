26ad623268170655aba92d2f7595d1fcc90b3d3e
root <root@DESKTOP-KQHN95E>
2023-12-21 01:03:09 +0700

    code naked_

--- src/naked_pairs.c
+++ src/naked_pairs.c
@@ -1,6 +1,90 @@
 #include "naked_pairs.h"
+#include<stdio.h>
+#include <stdlib.h>
+
+bool check_value(Cell *a, Cell *b) {
+    for(int val = 1; val<=BOARD_SIZE; val++) {
+        if(is_candidate(a, val)^is_candidate(b, val)) return false;
+    }
+    return (a->num_candidates==2 && b->num_candidates == 2);
+}
+
+void set_naked_pair(Cell **p_cell, Cell *a, Cell *b) {
+    for(int i=0; i<BOARD_SIZE; i++) {
+        if(p_cell[i]==a || p_cell[i]==b) continue;
+        for(int val = 1; val<=BOARD_SIZE; val++) {
+            if(is_candidate(a, val) && is_candidate(p_cell[i], val)) 
+                unset_candidate(p_cell[i], val);
+        }
+    }
+}
+
+int find_naked_pairs_rows(Cell **p_cell, int oke[][BOARD_SIZE*BOARD_SIZE], int id) {
+    int res = 0;
+    for(int i=0; i<BOARD_SIZE; i++) {
+        for(int j=i+1; j<BOARD_SIZE; j++) {
+            
+            int x = id*BOARD_SIZE+i;
+            int y = id*BOARD_SIZE+j;
+
+            if(!oke[x][y] && check_value(p_cell[i], p_cell[j])) {
+                //  printf("%d, %d, %d____\n", x, y, id);
+                res++;
+                oke[x][y] = 1;
+                set_naked_pair(p_cell, p_cell[i], p_cell[j]);
+            }
+        }
+    }
+    return res;
+}
+
+int find_naked_pairs_cols(Cell **p_cell, int oke[][BOARD_SIZE*BOARD_SIZE], int id) {
+    int res = 0;
+    for(int i=0; i<BOARD_SIZE; i++) {
+        for(int j=i+1; j<BOARD_SIZE; j++) {
+            int x = i*BOARD_SIZE+id;
+            int y = j*BOARD_SIZE+id;
+            if(!oke[x][y] && check_value(p_cell[i], p_cell[j])) {
+                res++;
+                oke[x][y] = 1;
+                set_naked_pair(p_cell, p_cell[i], p_cell[j]);
+            }
+        }
+    }
+    return res;
+}
+
+int find_naked_pairs_boxes(Cell **p_cell, int oke[][BOARD_SIZE*BOARD_SIZE], int id) {
+    int res = 0;
+    for(int i=0; i<BOARD_SIZE; i++) {
+        for(int j=i+1; j<BOARD_SIZE; j++) {
+
+            int x = 3*(id/3)*BOARD_SIZE+(i/3)*BOARD_SIZE+(id%3)*3+i%3;
+            int y = 3*(id/3)*BOARD_SIZE+(j/3)*BOARD_SIZE+(id%3)*3+j%3;
+        
+            if(!oke[x][y] && check_value(p_cell[i], p_cell[j])) {
+                // printf("%d, %d, %d\n", x, y, id);
+                res++;
+                oke[x][y] = 1;
+                set_naked_pair(p_cell, p_cell[i], p_cell[j]);
+            }
+        }
+    }
+    return res;
+}
+
 
 int naked_pairs(SudokuBoard *p_board)
 {
-    return 0;
+    int res = 0;
+    int oke[BOARD_SIZE*BOARD_SIZE][BOARD_SIZE*BOARD_SIZE] = {{0}};
+    
+    for(int i=0; i<BOARD_SIZE; i++) {
+        res += find_naked_pairs_rows(p_board->p_rows[i], oke, i);
+        res += find_naked_pairs_cols(p_board->p_cols[i], oke, i);
+        res += find_naked_pairs_boxes(p_board->p_boxes[i], oke, i);
+    }
+    
+    // free(oke);
+    return res;
 }
--- src/naked_triples.c
+++ src/naked_triples.c
@@ -1,6 +1,109 @@
 #include "naked_triples.h"
+#include<stdio.h>
+#include <stdlib.h>
+
+#define BOARD_SIZE1D 81
+
+bool check_val_triples(Cell *a, Cell *b, Cell *c) {
+    int cnt = 0;
+    for(int val = 1; val<=BOARD_SIZE; val++) {
+        if(is_candidate(a, val)|is_candidate(b, val) | is_candidate(c, val)) cnt++;
+    }
+    int size_a = a->num_candidates;
+    int size_b = b->num_candidates;
+    int size_c = c->num_candidates;
+    return (size_a >=2 && size_b >=2 && size_c >=2 &&
+            size_a <=3 && size_b <= 3 && size_c <=3 && 
+            cnt == 3 );
+}
+
+void set_naked_triples(Cell **p_cell, Cell *a, Cell *b, Cell *c) {
+    for(int i=0; i<BOARD_SIZE; i++) {
+        if(p_cell[i]==a || p_cell[i]==b || p_cell[i] == c) continue;
+        for(int val = 1; val<=BOARD_SIZE; val++) {
+            if(is_candidate(a, val) && is_candidate(p_cell[i], val)) 
+                unset_candidate(p_cell[i], val);
+            if(is_candidate(b, val) && is_candidate(p_cell[i], val)) 
+                unset_candidate(p_cell[i], val);
+            if(is_candidate(c, val) && is_candidate(p_cell[i], val)) 
+                unset_candidate(p_cell[i], val);
+        }
+    }
+}
+
+int find_naked_triples_rows(Cell **p_cell, int oke[][BOARD_SIZE1D][BOARD_SIZE1D], int id) {
+    int res = 0;
+    for(int i=0; i<BOARD_SIZE; i++) {
+        for(int j=i+1; j<BOARD_SIZE; j++) {
+            for(int k = j+1; k<BOARD_SIZE; k++) {
+                int x = id*BOARD_SIZE + i;
+                int y = id*BOARD_SIZE + j;
+                int z = id*BOARD_SIZE + k;
+                if(!oke[x][y][z] && check_val_triples(p_cell[i], p_cell[j], p_cell[k])) {
+                    res++;
+                    oke[x][y][z] = 1;
+                    set_naked_triples(p_cell, p_cell[i], p_cell[j], p_cell[k]);
+                }
+            }
+        }
+    }
+    return res;
+}
+
+int find_naked_triples_cols(Cell **p_cell, int oke[][BOARD_SIZE1D][BOARD_SIZE*BOARD_SIZE], int id) {
+    int res = 0;
+    for(int i=0; i<BOARD_SIZE; i++) {
+        for(int j=i+1; j<BOARD_SIZE; j++) {
+            for(int k = j+1; k<BOARD_SIZE; k++) {
+
+                int x = i*BOARD_SIZE + id;
+                int y = j*BOARD_SIZE + id;
+                int z = k*BOARD_SIZE + id;
+
+                if(!oke[x][y][z] && check_val_triples(p_cell[i], p_cell[j], p_cell[k])) {
+                    res++;
+                    oke[x][y][z] = 1;
+                    set_naked_triples(p_cell, p_cell[i], p_cell[j], p_cell[k]);
+                }
+            }
+        }
+    }
+    return res;
+}
+
+int find_naked_triples_boxes(Cell **p_cell, int oke[][BOARD_SIZE1D][BOARD_SIZE1D], int id) {
+    int res = 0;
+    for(int i=0; i<BOARD_SIZE; i++) {
+        for(int j=i+1; j<BOARD_SIZE; j++) {
+            for(int k = j+1; k<BOARD_SIZE; k++) {
+                
+                int x = 3*(id/3)*BOARD_SIZE+(i/3)*BOARD_SIZE+(id%3)*3+i%3;
+                int y = 3*(id/3)*BOARD_SIZE+(j/3)*BOARD_SIZE+(id%3)*3+j%3;
+                int z = 3*(id/3)*BOARD_SIZE+(k/3)*BOARD_SIZE+(id%3)*3+k%3;
+                
+                
+                if(!oke[x][y][z] && check_val_triples(p_cell[i], p_cell[j], p_cell[k])) {
+                    // printf("%d, %d, %d, %d\n", x, y, z, id);
+                    res++;
+                    oke[x][y][z] = 1;
+                    set_naked_triples(p_cell, p_cell[i], p_cell[j], p_cell[k]);
+                }
+            }
+        }
+    }
+    return res;
+}
 
 int naked_triples(SudokuBoard *p_board)
-{
-    return 0;
+{   
+    int res = 0;
+    int oke[BOARD_SIZE1D][BOARD_SIZE1D][BOARD_SIZE1D] = {{{0}}};
+    
+    for(int i =0; i<BOARD_SIZE; i++) {
+        res += find_naked_triples_rows(p_board->p_rows[i], oke, i);
+        res += find_naked_triples_cols(p_board->p_cols[i], oke, i);
+        res += find_naked_triples_boxes(p_board->p_boxes[i], oke, i);
+    }
+    
+    return res;
 }
